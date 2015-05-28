ecf040fa105464f9b451653403abfb31cd57408f
divine_child <tops@Tops31.local>
2015-05-28 10:05:42 +0530

    Initial Commit

---
+++ Urban96/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Urban96
+//
+//  Created by Tops on 5/28/15.
+//  Copyright (c) 2015 Tops. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
